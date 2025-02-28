25b749409881f23df92d2ee19b3ccb22e88c15bf
Can <canozcakal35@gmail.com>
2025-02-28 20:19:29 +0300

    Simple field creation and planting operations

---
+++ Assets/Script/TilemapClickHandler.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapClickHandler : MonoBehaviour
+{
+    public Tilemap tilemap;                   // Tilemap referansı
+    public TileBase farmlandTile;               // Farmland (tarla) tile asset'i
+    public EkinSecimManager ekinSecimManager;   // Ekim panelini yöneten manager
+    public TileConversionManager tileConversionManager; // Conversion modunu yöneten manager
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0;
+            Vector3Int tilePos = tilemap.WorldToCell(mouseWorldPos);
+            TileBase clickedTile = tilemap.GetTile(tilePos);
+            if (clickedTile == null) return;
+
+            // Conversion modu aktifse, conversion işlemleri geçerli; aksi halde normal mod
+            if (tileConversionManager != null && tileConversionManager.conversionModeActive)
+            {
+                // Eğer tıklanan tile farmland tile olarak belirlenmişse (isimleri karşılaştırıyoruz)
+                if (clickedTile.name == farmlandTile.name)
+                {
+                    ekinSecimManager.EkinSecimiAc(tilePos);
+                }
+                return;
+            }
+            // Normal modda, farmland tile ise ekim panelini aç
+            if (clickedTile.name == farmlandTile.name)
+            {
+                ekinSecimManager.EkinSecimiAc(tilePos);
+            }
+        }
+    }
+}
